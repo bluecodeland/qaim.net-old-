aad89ce70f28bc2bb75a62a766aa714a671d4284
unknown <mhtaeb@gmail.com>
2020-02-23 02:12:02 +0330

    farsi font working now

--- public/js/app.js
+++ public/js/app.js
@@ -37071,8 +37071,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\0dev\qaim.net\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\0dev\qaim.net\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\dev\qaim.net\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\dev\qaim.net\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="css/app.css">
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -45,18 +45,9 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 4VW;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
 
             .m-b-md {
                 margin-bottom: 30px;
@@ -81,19 +72,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    مدرسه عالی فقه و اصول حضرت قائم <br>
+                    واحد فنی و رسانه
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
             </div>
         </div>
     </body>
